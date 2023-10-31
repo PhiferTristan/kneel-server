72739634815cfc1e3263491bd76df4401a004034
PhiferTristan <phifer.tristan@outlook.com>
2023-10-31 16:11:23 -0500

    Added DELETE and POST methods to OrdersView

--- views/order_view.py
+++ views/order_view.py
@@ -19,3 +19,12 @@ class OrdersView():
             serialized_orders = json.dumps(orders)
 
             return handler.response(serialized_orders, status.HTTP_200_SUCCESS.value)
+
+    def update(self, handler, order_data, pk):
+        sql = "UPDATE Orders SET metal_id = ?, size_id = ?, style_id = ? WHERE id = ?"
+        row_updated = db_update(sql, (order_data['metal_id'], order_data['size_id'], order_data['style_id'], pk))
+
+        if row_updated > 0:
+            return handler.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
+        else:
+            return handler.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
