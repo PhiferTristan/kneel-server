424ea1559bf3a03816a0b90d6c4e570fb7e29b81
PhiferTristan <phifer.tristan@outlook.com>
2023-10-31 16:12:01 -0500

    Added POST, PUT, and DELETE methods to OrdersView

--- views/order_view.py
+++ views/order_view.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 from nss_handler import status
-from repository import db_get_single, db_get_all, db_update
+from repository import db_get_single, db_get_all, db_delete, db_create
 
 class OrdersView():
 
@@ -20,11 +20,20 @@ class OrdersView():
 
             return handler.response(serialized_orders, status.HTTP_200_SUCCESS.value)
 
-    def update(self, handler, order_data, pk):
-        sql = "UPDATE Orders SET metal_id = ?, size_id = ?, style_id = ? WHERE id = ?"
-        row_updated = db_update(sql, (order_data['metal_id'], order_data['size_id'], order_data['style_id'], pk))
+    def delete(self, handler, url):
+        row_deleted = db_delete("DELETE FROM Orders WHERE id =?", url['pk'])
 
-        if row_updated > 0:
+        if row_deleted > 0:
             return handler.response("", status.HTTP_204_SUCCESS_NO_RESPONSE_BODY.value)
         else:
             return handler.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
+
+    def create(self, handler, order_data):
+        sql = "INSERT INTO Orders (metal_id, size_id, style_id) VALUES (?, ?, ?)"
+
+        row_created = db_create(sql, (order_data['metal_id'], order_data['size_id'], order_data['style_id']))
+
+        if row_created > 0:
+            return handler.response("", status.HTTP_201_SUCCESS_CREATED.value)
+        else:
+            return handler.response("", status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value)
